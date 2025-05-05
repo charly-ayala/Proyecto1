f7752b22f4476f54beebe212e276a57b615a78fe
Carlos Ayala <2015966b@umich.mx>
2025-05-05 17:46:27 -0600

    Update Usuario.java

--- src/modelo/Usuario.java
+++ src/modelo/Usuario.java
@@ -1,4 +1,25 @@
 package modelo;
 
-public class Usuario {
+public abstract class Usuario {
+    protected String nombre;
+    protected String usuario;
+    protected String contrasena;
+
+    public Usuario(String nombre, String usuario, String contrasena) {
+        this.nombre = nombre;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
+
+    public boolean iniciarSesion(String usuario, String contrasena) {
+        return this.usuario.equals(usuario) && this.contrasena.equals(contrasena);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
 }
