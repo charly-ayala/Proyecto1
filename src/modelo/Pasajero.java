4884744f33607ca2965415012be9c02ae7a60005
Carlos Ayala <2015966b@umich.mx>
2025-05-05 17:45:58 -0600

    Update Pasajero.java

--- src/modelo/Pasajero.java
+++ src/modelo/Pasajero.java
@@ -1,4 +1,7 @@
 package modelo;
 
-public class Pasajero {
+public class Pasajero extends Usuario {
+    public Pasajero(String nombre, String usuario, String contrasena) {
+        super(nombre, usuario, contrasena);
+    }
 }
