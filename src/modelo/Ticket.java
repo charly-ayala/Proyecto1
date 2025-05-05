8ebba18643f891cf95d459b60fcf3b01c6dc672d
Carlos Ayala <2015966b@umich.mx>
2025-05-05 17:45:29 -0600

    Update Ticket.java

--- src/modelo/Ticket.java
+++ src/modelo/Ticket.java
@@ -1,4 +1,21 @@
 package modelo;
 
 public class Ticket {
+    private Vuelo vuelo;
+    private Pasajero pasajero;
+    private int cantidadLugares;
+
+    public Ticket(Vuelo vuelo, Pasajero pasajero, int cantidadLugares) {
+        this.vuelo = vuelo;
+        this.pasajero = pasajero;
+        this.cantidadLugares = cantidadLugares;
+    }
+
+    public void imprimir() {
+        System.out.println("----- Ticket de Reserva -----");
+        System.out.println("Pasajero: " + pasajero.getNombre());
+        System.out.println("Vuelo: " + vuelo.getOrigen() + " → " + vuelo.getDestino());
+        System.out.println("Lugares reservados: " + cantidadLugares);
+        System.out.println("-----------------------------");
+    }
 }
